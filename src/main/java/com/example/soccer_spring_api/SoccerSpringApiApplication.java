c6df28b065ca31671dcf00a9ab52eb6bf68e3ece
Bruno Cantacini <brunocantacini.ti@gmail.com>
2025-11-26 14:07:24 -0300

    feat: configurando aplicação

---
+++ src/main/java/com/example/soccer_spring_api/SoccerSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.soccer_spring_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerSpringApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/soccer_spring_api/SoccerSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soccer_spring_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
